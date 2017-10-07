ec1e71baae4d3b9899a00be399fe6283299dc11e
Vlad Vrabie <vladvra@yahoo.com>
2017-10-07 17:37:31 +0300

    added to main window one face of the cube

--- QtFlatVisualization/mainwindow.cpp
+++ QtFlatVisualization/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    face = new FaceCube2D(ui->gridLayout);
+    ui->gridLayout->addItem(face, 0, 0);
 }
 
 MainWindow::~MainWindow()
--- QtFlatVisualization/mainwindow.h
+++ QtFlatVisualization/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "facecube2d.h"
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +18,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    FaceCube2D *face;
 };
 
 #endif // MAINWINDOW_H
