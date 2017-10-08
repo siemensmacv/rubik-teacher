35f807af0ebffab3a1ffdd565850b8af010bf636
Vlad Vrabie <vladvra@yahoo.com>
2017-10-08 20:11:52 +0300

    added clases for managing flat cube movement

---
+++ QtFlatVisualization/qrubikcube6matrix.h
@@ -0,0 +1,17 @@
+#ifndef QRUBIKCUBE6MATRIX_H
+#define QRUBIKCUBE6MATRIX_H
+
+#include <QObject>
+
+class QRubikCube6Matrix : public QObject
+{
+    Q_OBJECT
+public:
+    explicit QRubikCube6Matrix(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // QRUBIKCUBE6MATRIX_H
