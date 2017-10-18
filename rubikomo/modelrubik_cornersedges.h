cfcb4d5161f0756a5b4593203fabd5a5869376ee
Vlad Vrabie <vladvra@yahoo.com>
2017-10-18 17:14:36 +0300

    defined header for Cubie model; started generic clockwise move

--- rubikomo/modelrubik_cornersedges.h
+++ rubikomo/modelrubik_cornersedges.h
@@ -12,10 +12,25 @@ class ModelRubik_CornersEdges
 public:
 	ModelRubik_CornersEdges();
 	~ModelRubik_CornersEdges();
+
+    void rotateClockwise(const RubikFace &face);
+    void rotateCounterClockwise(const RubikFace &face);
+
+    Corner getCorner(const Corner &position);
+    CornerOrientation getCornerOrientation(const Corner &position);
+
+    Edge getEdge(const Edge &position);
+    EdgeOrientation getEdgeOrientation(const Edge &position);
+
+    RubikFace getCenter(const RubikFace &position);
+private:
 	void init_corners();
 	void init_edges();
 	void init_centers();
-private:
+
+    void rotateFaceClockwise(int UL, int UR, int DR, int DL, const RubikFace &face);
+    void rotateFaceCounterClockwise(int UR, int UL, int DL, int DR, const RubikFace &face);
+
     Corner corner_position[8];
     Edge edge_position[12];
     RubikFace center[6];
