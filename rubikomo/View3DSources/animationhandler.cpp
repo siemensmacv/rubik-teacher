cf4b27c291bd7afd597662106fe29d46d240bfcc
Madalin Morcov <madalinmorcov@gmail.com>
2017-10-19 19:51:57 +0300

    animationhandler functions cpp

--- rubikomo/View3DSources/animationhandler.cpp
+++ rubikomo/View3DSources/animationhandler.cpp
@@ -9,3 +9,33 @@ AnimationHandler::AnimationHandler(Qt3DCore::QTransform *cuboidTransform):
 {
 
 }
+
+void AnimationHandler::startXAxisClockAnim()
+{
+
+}
+
+void AnimationHandler::startXAxisCClockAnim()
+{
+
+}
+
+void AnimationHandler::startYAxisClockAnim()
+{
+
+}
+
+void AnimationHandler::startYAxisCClockAnim()
+{
+
+}
+
+void AnimationHandler::startZAxisClockAnim()
+{
+
+}
+
+void AnimationHandler::startZAxisCClockAnim()
+{
+
+}
