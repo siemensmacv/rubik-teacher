952baf8b62a44e3e56081f52bdea3f70d8f5cd6d
DESKTOP-4KF8JEH\Andreea <pavel.andreea10@yahoo.com>
2017-10-21 20:03:34 +0300

    Color input

---
+++ Color_Input/color_input/colorinput.h
@@ -0,0 +1,25 @@
+#ifndef COLORINPUT_H
+#define COLORINPUT_H
+
+#include <QWidget>
+#include <QList>
+
+class ColorInput : public QWidget
+{
+
+public:
+    Q_OBJECT
+    QList<Qt::GlobalColor>colorList;
+    QList<int> rectColor;
+    QList<QRect> rectangles;
+public:
+    explicit ColorInput(QWidget *parent = nullptr);
+    void ColorInput::paintEvent(QPaintEvent *event) override;
+    void ColorInput::mousePressEvent(QMouseEvent *event) override;
+
+signals:
+
+public slots:
+};
+
+#endif // COLORINPUT_H
