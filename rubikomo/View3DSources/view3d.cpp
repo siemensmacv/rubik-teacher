7eb99a26eef1c879cd388b620b96429b34b79af1
Madalin Morcov <madalinmorcov@gmail.com>
2017-10-27 13:28:18 +0300

    camera front

--- rubikomo/View3DSources/view3d.cpp
+++ rubikomo/View3DSources/view3d.cpp
@@ -26,7 +26,7 @@ void View3D::initScene(){
     cameraEntity = view->camera();
 
     cameraEntity->lens()->setPerspectiveProjection(45.0f, 16.0f/9.0f, 0.1f, 100.0f);
-    cameraEntity->setPosition(QVector3D(5.0f, 5.0f, 5.0f));
+    cameraEntity->setPosition(QVector3D(3.0f, 4.0f, 5.0f));
     cameraEntity->setUpVector(QVector3D(0, 1, 0));
     cameraEntity->setViewCenter(QVector3D(0, 0, 0));
 
@@ -77,7 +77,7 @@ void View3D::onInput(int load)
 }
 
 void View3D::resetCamera(){
-    cameraEntity->setPosition(QVector3D(5.0f, 5.0f, 5.0f));
+    cameraEntity->setPosition(QVector3D(3.0f, 4.0f, 5.0f));
     cameraEntity->setUpVector(QVector3D(0.0f, 1.0f, 0.0f));
     cameraEntity->setViewCenter(QVector3D(0.0f, 0.0f, 0.0f));
 }
